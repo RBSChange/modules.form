ab46aeeaafa6bc57cc322c08a13c64c7a8722e87
Eric HAUSWALD <eric.hauswald@rbs.fr>
2011-07-25 12:00:11 +0200

    [NEW] Externalisation PHPTAL

--- lib/phptal/Captcha.php
+++ lib/phptal/Captcha.php
@@ -4,73 +4,57 @@
  * @author INTbonjF
  * 2007-08-02
  */
-class PHPTAL_Php_Attribute_CHANGE_captcha extends PHPTAL_Php_Attribute
+class PHPTAL_Php_Attribute_CHANGE_Captcha extends ChangeTalAttribute
 {
-	public function start()
+	/**
+	 * @see ChangeTalAttribute::getDefaultValues()
+	 */
+	protected function getDefaultValues()
 	{
-		$parameters = array();
-		$key = "'default'";
-		$expressions = $this->tag->generator->splitExpression($this->expression);
-		foreach ($expressions as $exp)
+		return array('key' => 'default');
+	}
+
+	/**
+	 * @see ChangeTalAttribute::getEvaluatedParameters()
+	 */
+	protected function getEvaluatedParameters()
+	{
+		return array('width', 'height', 'fontsize', 'fontdepth', 'maxlength', 'key');
+	}
+	
+	public static function renderCaptcha($parameters, $ctx)
+	{
+		if ($parameters['tagname'] === 'img')
 		{
-			list ($attribute, $value) = $this->parseSetExpression($exp);
-			$attribute = strtolower($attribute);
-			
-			switch ($attribute)
-			{
-				case 'width' :
-					$parameters['iw'] = $this->evaluate($value);
-					break;
-				case 'height' :
-					$parameters['ih'] = $this->evaluate($value);
-					break;
-				case 'fontsize' :
-					$parameters['fs'] = $this->evaluate($value);
-					break;
-				case 'fontdepth' :
-					$parameters['fd'] = $this->evaluate($value);
-					break;
-				case 'maxlength' :
-					$parameters['ml'] = $this->evaluate($value);
-					break;
-				case 'key' :
-					$key = $this->evaluate($value);
-					break;
-			}
+			self::renderImage($parameters, $ctx);
 		}
-		
-		switch (strtolower($this->tag->name))
+		elseif ($parameters['tagname'] === 'input')
 		{
-			case 'img' :
-				$this->tag->generator->doEcho('PHPTAL_Php_Attribute_CHANGE_captcha::renderImage(' . var_export($this->tag->attributes, true) . ', ' . var_export($parameters, true) . ', strval(' . $key . '))');
-				break;
-			case 'input' :
-				$this->tag->generator->doEcho('PHPTAL_Php_Attribute_CHANGE_captcha::renderInput(' . var_export($this->tag->attributes, true) . ', ' . var_export($parameters, true) . ', strval(' . $key . '))');
-				break;
+			self::renderInput($parameters, $ctx);
 		}
 	}
 	
-	private static function generateCaptchaCode($parameters, $key)
+	private static function generateCaptchaCode($parameters)
 	{
 		$generator = form_CaptchaGenerator::getInstance();
-		$generator->setKey($key);
+		$generator->setKey($parameters['key']);
 		foreach ($parameters as $name => $value)
 		{
 			switch ($name)
 			{
-				case 'ml' :
+				case 'maxlength' :
 					$generator->setCodeMaxLength(intval($value));
 					break;
-				case 'iw' :
+				case 'width' :
 					$generator->setWidth(intval($value));
 					break;
-				case 'ih' :
+				case 'height' :
 					$generator->setHeight(intval($value));
 					break;
-				case 'fs' :
+				case 'fontsize' :
 					$generator->setFontSize(intval($value));
 					break;
-				case 'fd' :
+				case 'fontdepth' :
 					$generator->setFontDepth(intval($value));
 					break;
 			}
@@ -78,9 +62,27 @@ class PHPTAL_Php_Attribute_CHANGE_captcha extends PHPTAL_Php_Attribute
 		$generator->generateCode();
 	}
 	
-	public function renderImage($attributes, $parameters, $key)
+	/**
+	 * @param array $parameters
+	 * @param PHPTAL_Context $ctx
+	 */
+	private static function renderImage($allParameters, $ctx)
 	{
-		// Get CAPTCHA parameters from the module form's preferences.
+		$parameterNames = $this->getEvaluatedParameters();
+		$parameters = array();
+		$attributes = array();
+		foreach ($allParameters as $name => $value) 
+		{
+			if (in_array($name, $parameterNames))
+			{
+				$parameters[$name] = $value;
+			}
+			else
+			{
+				$attributes[$name] = $value;
+			}
+		}
+		
 		$pref = ModuleService::getInstance()->getPreferencesDocument('form');
 		if ($pref !== null)
 		{
@@ -93,13 +95,15 @@ class PHPTAL_Php_Attribute_CHANGE_captcha extends PHPTAL_Php_Attribute
 				}
 			}
 		}
-		self::generateCaptchaCode($parameters, $key);
-		$parameters['key'] = $key;
-		$url = htmlentities(LinkHelper::getActionUrl('form', 'Captcha', $parameters), ENT_COMPAT, "utf-8");
+		self::generateCaptchaCode($parameters);
+		$url = LinkHelper::getActionLink('form', 'Captcha')
+			->setQueryParameters($parameters)
+			->setArgSeparator(f_web_HttpLink::ESCAPE_SEPARATOR)->getUrl();
+			
 		$code = '<span class="captcha"><input type="image" src="' . $url . '"';
 		if (!isset($attributes['title']))
 		{
-			$attributes['title'] = f_Locale::translate("&modules.form.bo.general.Captcha-click-to-have-another-one;");
+			$attributes['title'] = LocaleService::getInstance()->transFO("m.form.bo.general.captcha-click-to-have-another-one", array('ucf', 'attr'));
 		}
 		if (!isset($attributes['onclick']))
 		{
@@ -111,10 +115,24 @@ class PHPTAL_Php_Attribute_CHANGE_captcha extends PHPTAL_Php_Attribute
 		}
 		return $code . ' /></span>';
 	}
-	
-	public function renderInput($attributes, $parameters, $key)
+
+	/**
+	 * @param array $parameters
+	 * @param PHPTAL_Context $ctx
+	 */
+	private static function renderInput($allParameters, $ctx)
 	{
 		$code = '<input type="text" class="textfield" name="formParam[' . CAPTCHA_SESSION_KEY . ']"';
+		$parameterNames = array('width', 'height', 'fontsize', 'fontdepth', 'key');
+		$attributes = array();
+		foreach ($allParameters as $name => $value) 
+		{
+			if (!in_array($name, $parameterNames))
+			{
+				$attributes[$name] = $value;
+			}
+		}
+		
 		if (isset($attributes['name']))
 		{
 			unset($attributes['name']);
@@ -129,16 +147,12 @@ class PHPTAL_Php_Attribute_CHANGE_captcha extends PHPTAL_Php_Attribute
 		}
 		if (!isset($attributes['title']))
 		{
-			$attributes['title'] = f_Locale::translate("&modules.form.bo.general.Captcha-help;");
+			$attributes['title'] = LocaleService::getInstance()->transFO("m.form.bo.general.captcha-help", array('ucf', 'attr'));
 		}
 		foreach ($attributes as $name => $value)
 		{
 			$code .= ' ' . $name . '="' . str_replace('"', '\"', $value) . '"';
 		}
 		return $code . '/>';
-	}
-	
-	public function end()
-	{
 	}
 }
