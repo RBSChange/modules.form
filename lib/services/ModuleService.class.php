abe830006b3cedc8be56af3289fd5fdf84f49fec
Loïc Couturier <loic.couturier@rbs.fr>
2012-08-02 15:55:16 +0200

    [UPDATE] Clean bad comment

--- lib/services/ModuleService.class.php
+++ lib/services/ModuleService.class.php
@@ -22,12 +22,4 @@ class form_ModuleService extends ModuleBaseService
 		return self::$instance;
 	}
 	
-	/**
-	 * @param Integer $documentId
-	 * @return f_persistentdocument_PersistentTreeNode
-	 */
-//	public function getParentNodeForPermissions($documentId)
-//	{
-//		// Define this method to handle permissions on a virtual tree node. Example available in list module.
-//	}
 }
